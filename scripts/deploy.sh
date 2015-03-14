42cae8979a2ed1546a0ca25d7a3526df75f47ca0
kai <kaizeng@eecs.berkeley.edu>
2015-03-14 23:53:59 +0000

    fix bug in deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,6 @@ build/sbt -DskipTests -Phive -Phive-thriftserver assembly && \
 ~/spark/sbin/stop-all.sh && \
 cp assembly/target/scala-2.10/spark-assembly*.jar ~/spark/lib/spark-assembly-1.2.1-hadoop1.0.4.jar && \
 cp examples/target/scala-2.10/spark-examples*.jar ~/spark/lib/spark-examples-1.2.1-hadoop1.0.4.jar && \
+~/spark-ec2/copy-dir --delete ~/spark/conf && \
 ~/spark-ec2/copy-dir --delete ~/spark/lib && \
 ~/spark/sbin/start-all.sh
