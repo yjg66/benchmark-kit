b49c64a7f596edd5bda0b898698450dc7041b3d6
kai <kaizeng@eecs.berkeley.edu>
2015-03-29 21:23:29 -0700

    improve benchmark.sh

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,13 +1,14 @@
-option="benchmark"
-if [[ $# -gt 0 ]]
-then
-  option+=$1
-fi
+#!/bin/bash
 
 cd /mnt/bootstrap-sql
 
-for q in 1 10 11 12 14 16 17 18 19 20 22 3 5 6 7 8 9
+for option in benchmark
 do
-	./bin/spark-submit --class org.apache.spark.examples.sql.hive.SQLSuite examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar $option $q
-	../benchmark-kit/scripts/clean-work.sh
+	for q in 1 10 11 12 14 16 17 18 19 20 22 3 5 6 7 8 9
+	do
+		./bin/spark-submit --class org.apache.spark.examples.sql.hive.SQLSuite \
+			examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar \
+			$option $q
+		../benchmark-kit/scripts/clean-work.sh
+	done
 done
