33ec8ae5c792fd60548ed406c74608734d221be9
kai <kaizeng@eecs.berkeley.edu>
2015-03-31 21:55:32 -0700

    add blksz experiments

---
+++ experiments/blksz.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd /mnt/bootstrap-sql
+
+for blksz in 32k 128k 512k 2048k
+do
+	echo "REMARK: set block size to $blksz tuples per block..."
+
+	./bin/spark-submit --class org.apache.spark.examples.sql.hive.SQLSuite \
+		examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar \
+		drop
+
+	./bin/spark-submit --class org.apache.spark.examples.sql.hive.SQLSuite \
+		examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar \
+		create-parquet parquet-$blksz
+
+	for option in run-0 run-1
+	do
+		for j in 1 2
+		do
+			for q in 1 10 11 12 14 16 17 18 19 20 22 3 5 6 7 8 9
+			do
+				./bin/spark-submit --class org.apache.spark.examples.sql.hive.SQLSuite \
+					examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar \
+					$option $q 1
+				../benchmark-kit/scripts/clean-work.sh
+			done
+		done
+	done
+done
