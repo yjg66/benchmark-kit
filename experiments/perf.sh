d0b43be6a4937a64ff3bbd5ba6f8421a2b9f65a8
kai <kaizeng@eecs.berkeley.edu>
2015-04-19 04:20:13 -0700

    add perf.sh

---
+++ experiments/perf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /mnt/bootstrap-sql
+
+for option in run-1 run-0 run-2
+do
+	for j in 1 2
+	do
+		for q in 1 10 11 12 14 16 17 18 19 20 22 3 5 6 7 8 9
+		do
+			./bin/spark-submit --class org.apache.spark.examples.sql.hive.SQLSuite \
+				examples/target/scala-2.10/spark-examples-1.3.0-SNAPSHOT-hadoop1.0.4.jar \
+				$option $q 1
+			../benchmark-kit/scripts/clean-work.sh
+		done
+	done
+done
