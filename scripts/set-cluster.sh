b2cecfbc4e74eb72db6b66b09d6296d33dadfc81
kai <kaizeng@eecs.berkeley.edu>
2015-05-14 12:51:22 -0700

    update set-cluster

--- scripts/set-cluster.sh
+++ scripts/set-cluster.sh
@@ -1,6 +1,6 @@
-if [[ $# -ne 3 ]]
+if [[ $# -lt 2 ]]
 then
-  echo "Usage: set-cluster [block size in MB] [memory in GB] [dataset scale]"
+  echo "Usage: set-cluster [dataset] [dataset scale] [memory in GB]"
 else
   # stop all
   /mnt/benchmark-kit/scripts/kill-all.sh
@@ -25,9 +25,7 @@ else
     chmod a+x /usr/local/bin/rsub
   fi
   
-  BLOCKSIZE=$(($1 * 1024 * 1024))
   sed -i 's/vol/vol0/g' ~/persistent-hdfs/conf/core-site.xml
-  sed -i "s/134217728/$BLOCKSIZE/g" ~/persistent-hdfs/conf/hdfs-site.xml
 
   ~/spark-ec2/copy-dir --delete ~/persistent-hdfs/conf
 
@@ -39,24 +37,35 @@ else
     ~/persistent-hdfs/bin/hadoop namenode -format
   fi
   
+  DATASET=$1
+  DATASCALE=$2
+  MEMSIZE=56
+  if [[ $# -eq 3 ]]
+  then
+    MEMSIZE=$3
+  fi
+
   # set up spark
+  # pointing to persistent hdfs
   sed -i 's/ephemeral/persistent/g' ~/spark/conf/spark-env.sh
-  sed -i 's/,\/mnt2\/spark//g' ~/spark/conf/spark-env.sh
-  echo "export SPARK_WORKER_DIR=/mnt/spark/work" >> ~/spark/conf/spark-env.sh
-
-  cp ~/persistent-hdfs/conf/core-site.xml ~/spark/conf/core-site.xml
-
   sed -i 's/ephemeral/persistent/g' ~/spark/conf/spark-defaults.conf
-  sed -i "s/spark.executor.memory[ \t].*$/spark.executor.memory $2g/g" ~/spark/conf/spark-defaults.conf
+  cp ~/persistent-hdfs/conf/core-site.xml ~/spark/conf/core-site.xml
+  # ?
+  # sed -i 's/,\/mnt2\/spark//g' ~/spark/conf/spark-env.sh
+  # redirect log folder
+  echo "export SPARK_WORKER_DIR=/mnt/spark/work" >> ~/spark/conf/spark-env.sh
+  # mem size
+  sed -i "s/spark.executor.memory[ \t].*$/spark.executor.memory $(($MEMSIZE))g/g" ~/spark/conf/spark-defaults.conf
+  echo "spark.driver.memory $(($MEMSIZE / 2))g" >> ~/spark/conf/spark-defaults.conf
+  # num of executors
   echo "spark.number.executors `cat ~/spark/conf/slaves | wc -l`" >> ~/spark/conf/spark-defaults.conf
+  # consolidate files
   echo "spark.shuffle.consolidateFiles true" >> ~/spark/conf/spark-defaults.conf
-  echo "spark.driver.memory $(($2 / 2))g" >> ~/spark/conf/spark-defaults.conf
-  echo "spark.sql.autoBroadcastJoinThreshold 52428800" >> ~/spark/conf/spark-defaults.conf
-  echo "spark.sql.online.test.scale $3" >> ~/spark/conf/spark-defaults.conf
-  echo "spark.sql.broadcastTimeout 1200" >> ~/spark/conf/spark-defaults.conf
+  # other configs
+  cat /mnt/benchmark-kit/$DATASET/conf/$DATASCALE >> ~/spark/conf/spark-defaults.conf
 
   ~/spark-ec2/copy-dir --delete ~/spark/conf
 
-  # restart
+  # restart all
   /mnt/benchmark-kit/scripts/start-all.sh
 fi    
--- scripts/set-ssh.sh
+++ scripts/set-ssh.sh
@@ -1,10 +1,8 @@
 while read line
 do
   ssh $line 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4E+/a2EfcwolyMCZDoj6cLMJjs1VKBLybnsdO7OAVWNzan8j3CYQuyjGw3ozz6riy2nAmqXzSytAEqLYRF8Mci+Y2nLtqNy/xPW7tkaO/yq9wGIfP2z9tDzlGNQTuG+GQ+0/Ia8dqHnXLXnd7l6pFKuotqUpmIvNmXMIchTLKFOpIipGQFJwF7dqoFS71q+4YO8KB0/NyB1tMuwz2gSTBX+pIb3NJ89HRMiLSHZ56D/7yMCbSH0g0Y639YNPvJ+RQgPA7lkuHiWN3t63lLfgZnmmfYjYq8Hbke8gkSkuFxjgMU+oUswHYZd5/W9DqY2i1kmYuvX5CWnOQ4rdmhygX Kai@Lenovo-PC" >> ~/.ssh/authorized_keys' < /dev/null
-  ssh $line 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKiDdqu1kfmDMXgZ7SyisPm9GpLis9b14yKXV5QyaU5Iqk6NpDgEfq/jNIXusofPkwx77L0HREwhRl3we8ExRTbyttFaawfJ4GHQeAjt8NpuTEaXrPQMOUgP3pm1Cw70zicQhO1rBsVrM+Z0lGwVXip7Ylhh83MBAwlXhZSe9eq5HVzc8U1vr4gfHKLa/6TclhwVIoEaqA48ZpgHMHNziGCMpHrvTgw8wwD9V9OiwEANnNcMOK2GYVhCb04kvwjDa3VqT+YZmeUchTLxQ0JDAqGosINyEG0tnfbeuBTX7R4MBmrWmd5crBl5wwlnnoqHFJjMNucV4bs84h6/vhGovL manu@laptop.local" >> ~/.ssh/authorized_keys' < /dev/null
 done < ~/persistent-hdfs/conf/masters
 while read line
 do
   ssh $line 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4E+/a2EfcwolyMCZDoj6cLMJjs1VKBLybnsdO7OAVWNzan8j3CYQuyjGw3ozz6riy2nAmqXzSytAEqLYRF8Mci+Y2nLtqNy/xPW7tkaO/yq9wGIfP2z9tDzlGNQTuG+GQ+0/Ia8dqHnXLXnd7l6pFKuotqUpmIvNmXMIchTLKFOpIipGQFJwF7dqoFS71q+4YO8KB0/NyB1tMuwz2gSTBX+pIb3NJ89HRMiLSHZ56D/7yMCbSH0g0Y639YNPvJ+RQgPA7lkuHiWN3t63lLfgZnmmfYjYq8Hbke8gkSkuFxjgMU+oUswHYZd5/W9DqY2i1kmYuvX5CWnOQ4rdmhygX Kai@Lenovo-PC" >> ~/.ssh/authorized_keys' < /dev/null
-  ssh $line 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKiDdqu1kfmDMXgZ7SyisPm9GpLis9b14yKXV5QyaU5Iqk6NpDgEfq/jNIXusofPkwx77L0HREwhRl3we8ExRTbyttFaawfJ4GHQeAjt8NpuTEaXrPQMOUgP3pm1Cw70zicQhO1rBsVrM+Z0lGwVXip7Ylhh83MBAwlXhZSe9eq5HVzc8U1vr4gfHKLa/6TclhwVIoEaqA48ZpgHMHNziGCMpHrvTgw8wwD9V9OiwEANnNcMOK2GYVhCb04kvwjDa3VqT+YZmeUchTLxQ0JDAqGosINyEG0tnfbeuBTX7R4MBmrWmd5crBl5wwlnnoqHFJjMNucV4bs84h6/vhGovL manu@laptop.local" >> ~/.ssh/authorized_keys' < /dev/null
 done < ~/persistent-hdfs/conf/slaves
